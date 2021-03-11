62aa6033dbe8af2e5dff6cd42e3f898474252484
Swarnim Pratap Singh <swarnim132@gmail.com>
2021-03-12 00:38:31 +0530

    Lamda function initialized

---
+++ functions/app.js
@@ -0,0 +1,35 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../keys/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://forum-8f2e6.firebaseio.com",
+});
+
+exports.handler = async (event, context, callback) => {
+  const firestore = admin.firestore();
+
+  let collections = await firestore.collection("users").get();
+
+  let doc = collections.docs[0].ref;
+
+  await doc
+    .update({
+      relation_status: "single",
+    })
+    .catch((err) => console.log(err));
+
+  doc = await doc.get();
+  console.log(doc.data());
+
+  return (
+    null,
+    {
+      statusCode: 200,
+      body: JSON.stringify({
+        text: "test",
+      }),
+    }
+  );
+};
