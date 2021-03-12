f5706d01a77017be9053bc12f758a0388069efc6
Swarnim Pratap Singh <swarnim132@gmail.com>
2021-03-12 20:40:13 +0530

    removed ultra-high files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-import React, { Component } from "react";
-import { Admin, Resource, EditGuesser } from "react-admin";
-import { UserList, UserEdit } from "./components/users";
-import { UserCreate } from "./components/admin";
-import jsonServerProvider from "ra-data-json-server";
-import authProvider from "./components/authProvider";
-// const express = require("express");
-// const app = express();
-// require("dotenv/config");
+import React from "react";
 
-// // PORT
-// const PORT = 5000;
-
-// // middlewares
-// app.use(express.json());
-
-// // ROUTES
-
-// // listen
-// app.listen(PORT, () => {
-//   console.log(`listeninig on http://localhost:${PORT}`);
-// });
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-
-class App extends Component {
-  render() {
-    return (
-      <Admin dataProvider={dataProvider} authProvider={authProvider}>
-        <Resource
-          name="users"
-          list={UserList}
-          edit={UserEdit}
-          create={UserCreate}
-        />
-      </Admin>
-    );
-  }
+function App() {
+  return <div>Hello world</div>;
 }
+
 export default App;
