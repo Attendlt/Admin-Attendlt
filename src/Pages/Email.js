d6526011abca6f59f66c69138c316045fd2630d4
vimalgupta24 <vimar4bsa@gmail.com>
2021-03-12 16:33:58 +0000

    email pages component

---
+++ src/Pages/Email.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import './Signin.css';
+
+function SEmail() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email && password) {
+            console.log("Login Successful!!");
+    }
+  };
+
+  return (<div>
+          <div className="form">
+        <Form onSubmit={handleSubmit} className="form">
+          <Form.Group>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </div>
+      </div>
+  );
+}
+
+export default SEmail;
--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import './Signin.css';
-
+import E from './Email';
 function Signin() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (email && password) {
-            console.log("Login Successful!!");
-    }
-  };
-
   return (
     <div className="poster-signin">
       <div className="container mt-4" style={{ width: "40%", padding: "10%" ,alignContent:'center',alignItems:'center'}}>
@@ -31,33 +21,13 @@ function Signin() {
         >
           To Login In Your Account, Fill the following details!
         </h3>
-        <Form onSubmit={handleSubmit} className="form">
-          <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+          <E/>
           <Button
             type="submit"
             style={{ width: "45%", background: "#ff3300" }}
           >
             Login
           </Button>
-        </Form>
       </div>
       </div>
     </div>
