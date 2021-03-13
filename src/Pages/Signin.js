45e609fc84cfd1489498ce6de64c084cc7dddddf
vimalgupta24 <vimar4bsa@gmail.com>
2021-03-13 07:25:44 +0000

    work no 3 on inst.js

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,14 +1,12 @@
 import React from "react";
 // import './Signin.css';
-import E from './Email';
-import I from './Inst';
+import Email from './Email';
 function Signin() {
   return (
     <div className="poster-signin" style={{backgroundSize:"cover"}}>
       <div className="container mt-4" style={{ borderRadius:"30px", height:"50%",margin:"auto", width:"50%",boxSizing:"border-box"}}>
-          <E/>
+          <Email/>
           &nbsp;
-          <I/>
       </div>
     </div>
   );
